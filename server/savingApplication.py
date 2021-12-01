0df66b45879259c094030f6904e41cf882ec2b76
StefanAraujo <stefanaraujo@gmail.com>
2021-11-30 20:10:39 -0500

    Appending to json file for tweets

--- server/savingApplication.py
+++ server/savingApplication.py
@@ -23,4 +23,21 @@ def ifthenwriteToFile(if_items,then_items,name):
     with open("result.json", 'w') as file:
          json.dump( data, file )
 
-ifthenwriteToFile(['heeloo','hello','yes'],['y','a','b'],"yess")
+def orToFile(if_items,then_items,name):
+    data = []
+    #check if file is empty
+    if((os.stat('tweetertester.json').st_size!=0)): 
+        with open("tweet.json", "r") as f:  
+            data = json.load(f)
+        print(data)
+    # data['yeet'] = "hello"
+    # with open("tweetertester.json", "w") as f:  
+    #     f.write(simplejson.dumps(simplejson.loads(data), indent=4, sort_keys=True))
+    temp = dict()
+    temp['OR'] = if_items
+    temp ['NAME'] = name
+
+    data.append(temp)
+    print(data)
+    with open("result.json", 'w') as file:
+         json.dump( data, file )
