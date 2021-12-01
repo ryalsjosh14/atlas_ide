17d371fc60f8289398a11645ac2ebad213d3efb5
Giovanni Ferioli <gferioli0418@gmail.com>
2021-12-01 13:13:04 -0500

    waiting for backend eendpoints for buttos

--- atlas_iot/src/tabs/Apps.js
+++ atlas_iot/src/tabs/Apps.js
@@ -3,50 +3,93 @@ import Button from "react-bootstrap/Button";
 
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
 
-let itemsFromBackend = {
-	App1: { Status: "success", Action: "Activate" },
-	App2: { Status: "success", Action: "Activate" },
-	App3: { Status: "success", Action: "Activate" },
-	App4: { Status: "success", Action: "Activate" },
-	App5: { Status: "success", Action: "Activate" },
-	App6: { Status: "success", Action: "Activate" },
-	App7: { Status: "success", Action: "Activate" },
-};
+const itemsFromBackend = [
+	{ Name: "App1", Status: "success", Action: "Activate" },
+	{ Name: "App2", Status: "success", Action: "Activate" },
+	{ Name: "App3", Status: "success", Action: "Activate" },
+	{ Name: "App4", Status: "success", Action: "Activate" },
+	{ Name: "App5", Status: "success", Action: "Activate" },
+	{ Name: "App6", Status: "success", Action: "Activate" },
+	{ Name: "App7", Status: "success", Action: "Activate" },
+];
 
 function Apps() {
 	const [Apps, setApps] = useState(itemsFromBackend);
+	const [show, setShow] = useState(false);
 
-	const handleActivateClick = (e) => {
-		console.log(e);
-		// x = { _name: { Status: "warning", Action: "Stop" } };
-		// setApps({ ...Apps, {x} });
+	const handleActivateClick = (Action, index) => {
+		const tempApps = [...Apps];
+		console.log(Array.isArray(tempApps));
+		if (Action == "Activate") {
+			tempApps[index].Status = "warning";
+			tempApps[index].Action = "Stop";
+			console.log(Array.isArray(tempApps));
+
+			setApps(tempApps);
+		} else {
+			tempApps[index].Status = "success";
+			tempApps[index].Action = "Activate";
+			setApps(tempApps);
+		}
+		console.log(typeof Apps);
 	};
-	let x = Object.entries(Apps).map(([el, val], index) => {
+	console.log(Apps);
+	const handleDeleteClick = (index) => {
+		const tempApps = [...Apps];
+		tempApps.splice(index, 1);
+		setApps(tempApps);
+
+		console.log("write acios request");
+		// TODO: Write axios request
+	};
+	const handleSaveClick = (index) => {
+		console.log("write acios request");
+		// TODO: Write axios request
+	};
+
+	const x = Apps.map((el, index) => {
 		return (
-			<div key={el}>
+			<div key={el.name}>
 				<Card>
 					<Card.Body style={{ display: "flex", flexDirection: "row" }}>
-						<Card.Title style={{ flex: 1, margin: 10 }}>{el}</Card.Title>
+						<Card.Title style={{ flex: 1, margin: 10 }}>{el.Name}</Card.Title>
 
 						<Button
-							variant={val.Status}
+							variant={el.Status}
 							style={{ flex: 1, margin: 10 }}
-							onClick={handleActivateClick}
+							onClick={() => handleActivateClick(el.Action, index)}
 						>
-							{val.Action}
+							{el.Action}
 						</Button>
 
-						<Button variant="danger" style={{ flex: 1, margin: 10 }}>
+						<Button
+							variant="danger"
+							style={{ flex: 1, margin: 10 }}
+							onClick={() => handleDeleteClick(el)}
+						>
 							Delete
 						</Button>
-						<Button variant="primary" style={{ flex: 1, margin: 10 }}>
+						<Button
+							variant="primary"
+							style={{ flex: 1, margin: 10 }}
+							onClick={() => handleSaveClick(el)}
+						>
 							Save
 						</Button>
-						<Button variant="secondary" style={{ flex: 1, margin: 10 }}>
+						<Button
+							variant="secondary"
+							style={{ flex: 1, margin: 10 }}
+							onClick={() => handleLogClick(el)}
+						>
 							Log
 						</Button>
-						<Button variant="dark" style={{ flex: 1, margin: 10 }}>
+						<Button
+							variant="dark"
+							style={{ flex: 1, margin: 10 }}
+							onClick={() => handleSaveClick(el)}
+						>
 							Output
 						</Button>
 					</Card.Body>
@@ -54,8 +97,38 @@ function Apps() {
 			</div>
 		);
 	});
+	const handleClose = () => setShow(false);
+	const handleLogClick = () => setShow(true);
 	return (
 		<div>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Log</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<ol>
+						<li>1:07: Started App1</li>
+						<li>1:10: Started App2</li>
+						<li>1:11: Started App3</li>
+						<li>1:12: Started App4</li>
+						<li>1:13: Started App5</li>
+						<li>1:17: Ended App1</li>
+						<li>1:20: Ended App2</li>
+						<li>1:21: Ended App3</li>
+						<li>1:22: Ended App4</li>
+						<li>1:23: Ended App5</li>
+					</ol>
+				</Modal.Body>
+
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleClose}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
 			<Form.Group controlId="formFile" className="mb-3">
 				<Form.Label>Upload File</Form.Label>
 				<Form.Control type="file" />
